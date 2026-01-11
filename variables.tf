83f0a8eea98d6604077b521f0952bae5af47856c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:32:08 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "batch_applications" {
+  description = <<EOT
+Map of batch_applications, attributes below
+Required:
+    - account_name
+    - name
+    - resource_group_name
+Optional:
+    - allow_updates
+    - default_version
+    - display_name
+EOT
+
+  type = map(object({
+    account_name        = string
+    name                = string
+    resource_group_name = string
+    allow_updates       = optional(bool, true)
+    default_version     = optional(string)
+    display_name        = optional(string)
+  }))
+}
+
