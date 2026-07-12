9b70ab80463239fcdf40447394b105847fb87dd0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:11 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "batch_applications" {
-  description = "All batch_application resources"
-  value       = azurerm_batch_application.batch_applications
-}
 output "batch_applications_account_name" {
-  description = "List of account_name values across all batch_applications"
-  value       = [for k, v in azurerm_batch_application.batch_applications : v.account_name]
+  description = "Map of account_name values across all batch_applications, keyed the same as var.batch_applications"
+  value       = { for k, v in azurerm_batch_application.batch_applications : k => v.account_name }
 }
 output "batch_applications_allow_updates" {
-  description = "List of allow_updates values across all batch_applications"
-  value       = [for k, v in azurerm_batch_application.batch_applications : v.allow_updates]
+  description = "Map of allow_updates values across all batch_applications, keyed the same as var.batch_applications"
+  value       = { for k, v in azurerm_batch_application.batch_applications : k => v.allow_updates }
 }
 output "batch_applications_default_version" {
-  description = "List of default_version values across all batch_applications"
-  value       = [for k, v in azurerm_batch_application.batch_applications : v.default_version]
+  description = "Map of default_version values across all batch_applications, keyed the same as var.batch_applications"
+  value       = { for k, v in azurerm_batch_application.batch_applications : k => v.default_version }
 }
 output "batch_applications_display_name" {
-  description = "List of display_name values across all batch_applications"
-  value       = [for k, v in azurerm_batch_application.batch_applications : v.display_name]
+  description = "Map of display_name values across all batch_applications, keyed the same as var.batch_applications"
+  value       = { for k, v in azurerm_batch_application.batch_applications : k => v.display_name }
 }
 output "batch_applications_name" {
-  description = "List of name values across all batch_applications"
-  value       = [for k, v in azurerm_batch_application.batch_applications : v.name]
+  description = "Map of name values across all batch_applications, keyed the same as var.batch_applications"
+  value       = { for k, v in azurerm_batch_application.batch_applications : k => v.name }
 }
 output "batch_applications_resource_group_name" {
-  description = "List of resource_group_name values across all batch_applications"
-  value       = [for k, v in azurerm_batch_application.batch_applications : v.resource_group_name]
+  description = "Map of resource_group_name values across all batch_applications, keyed the same as var.batch_applications"
+  value       = { for k, v in azurerm_batch_application.batch_applications : k => v.resource_group_name }
 }
 
